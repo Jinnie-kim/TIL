1805712bd8f57d2e65d434c11a8b536f6bd7021f
Jinnie <hjk7639@gmail.com>
2022-12-25 15:05:53 +0900

    타입스크립트 Enum

--- TypeScriptUdemy/app.ts
+++ TypeScriptUdemy/app.ts
@@ -2,22 +2,39 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; // tuple type
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuple type
+// } = {
+//   name: "Jinnie",
+//   age: 24,
+//   hobbies: ["Sports", "Reading"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
   name: "Jinnie",
   age: 24,
   hobbies: ["Sports", "Reading"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-person.role.push("admin");
+// person.role.push("admin");
 // person.role[1] = 10;
 
-person.role = [0, "hello"];
+// person.role = [0, "hello"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports", "Reading"];
@@ -27,4 +44,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); get an Error!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
