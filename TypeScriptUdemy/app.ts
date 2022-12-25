8280eb1567b7194c17b766a8c2b8536c4aa4fe3b
Jinnie <hjk7639@gmail.com>
2022-12-25 14:14:02 +0900

    타입스크립트 Array

--- TypeScriptUdemy/app.ts
+++ TypeScriptUdemy/app.ts
@@ -5,6 +5,15 @@
 const person = {
   name: "Jinnie",
   age: 24,
+  hobbies: ["Sports", "Reading"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports", "Reading"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); get an Error!
+}
