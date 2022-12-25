c2cbd5277ad8aeb7a2e3953ed70174fa5c0612c2
Jinnie <hjk7639@gmail.com>
2022-12-25 14:31:09 +0900

    타입스크립트 Tuple

--- TypeScriptUdemy/app.ts
+++ TypeScriptUdemy/app.ts
@@ -2,11 +2,22 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // tuple type
+} = {
   name: "Jinnie",
   age: 24,
   hobbies: ["Sports", "Reading"],
+  role: [2, "author"],
 };
+
+person.role.push("admin");
+// person.role[1] = 10;
+
+person.role = [0, "hello"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports", "Reading"];
