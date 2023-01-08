fbe52089a405eb6c83e938b315f6aefc064f11df
Jinnie <hjk7639@gmail.com>
2023-01-08 17:17:49 +0900

    TS union

--- TypeScriptUdemy/app.ts
+++ TypeScriptUdemy/app.ts
@@ -1,51 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // tuple type
-// } = {
-//   name: "Jinnie",
-//   age: 24,
-//   hobbies: ["Sports", "Reading"],
-//   role: [2, "author"],
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = "ADMIN",
-  READ_ONLY = 100,
-  AUTHOR = 200,
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const person = {
-  name: "Jinnie",
-  age: 24,
-  hobbies: ["Sports", "Reading"],
-  role: Role.ADMIN,
-};
+const combinedAges = combine(29, 24);
+console.log(combinedAges);
 
-// person.role.push("admin");
-// person.role[1] = 10;
-
-// person.role = [0, "hello"];
-
-let favoriteActivities: any[];
-favoriteActivities = ["Sports", "Reading"];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); get an Error!
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log("is author");
-}
+const combinedNames = combine("Harry", "Louis");
+console.log(combinedNames);
---
+++ TypeScriptUdemy/objs-array-enums.ts
@@ -0,0 +1,51 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuple type
+// } = {
+//   name: "Jinnie",
+//   age: 24,
+//   hobbies: ["Sports", "Reading"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
+  name: "Jinnie",
+  age: 24,
+  hobbies: ["Sports", "Reading"],
+  role: Role.ADMIN,
+};
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "hello"];
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", "Reading"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); get an Error!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
+}
