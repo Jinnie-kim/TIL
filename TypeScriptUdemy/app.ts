be744b94ce3d47ab044dfe419ed6f12f6b403b89
Jinnie <hjk7639@gmail.com>
2023-01-12 21:49:49 +0900

    TS FC types & callback

--- TypeScriptUdemy/app.ts
+++ TypeScriptUdemy/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 console.log(printResult(add(5, 12)));
 
 let combineValues: (a: number, b: number) => number;
@@ -16,3 +21,8 @@ combineValues = add;
 
 console.log(combineValues(8, 8));
 // let someValue: undefined;
+
+addAndHandler(10, 20, (result) => {
+  console.log(result);
+  return result;
+});
