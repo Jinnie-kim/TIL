1789be158c3e97292a231342dfa997788768104f
Jinnie <hjk7639@gmail.com>
2023-01-29 21:36:47 +0900

    useRef를 이용하여 input 입력값 받기

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useRef } from "react";
 
 const NewTodo = () => {
+  const todoTextInput = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    const enteredText = todoTextInput.current!.value;
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInput} />
       <button type="submit">Add Todo</button>
     </form>
   );
