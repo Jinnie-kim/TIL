1ce36d918678af8ddf41916b62350de8b86ad57e
Jinnie <hjk7639@gmail.com>
2023-01-29 22:03:58 +0900

    새로운 todo를 만드는 컴포넌트를 추가

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,15 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const addTodoHandler = (todoText: string) => {};
+
   return (
-    <div className="App">
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos itmes={todos} />
     </div>
   );
