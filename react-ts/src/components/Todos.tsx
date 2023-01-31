e6a8a486607bc2ec0c5de8cdb2abb09ce309f013
Jinnie <hjk7639@gmail.com>
2023-01-31 23:54:17 +0900

    Todo 삭제 함수 추가

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -6,13 +6,18 @@ import classes from "./Todos.module.css";
 type Props = {
   children?: React.ReactNode;
   itmes: Todo[];
+  onRemoveTodo: (todoId: string) => void;
 };
 
 const Todos: React.FC<React.PropsWithChildren<Props>> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.itmes.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
