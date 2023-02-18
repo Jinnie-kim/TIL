0b46dfb67ab0c837bc17cb7db27adf29be215df0
Jinnie <hjk7639@gmail.com>
2023-02-18 21:07:55 +0900

    TS overriding properties & protected

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -4,7 +4,7 @@
 class Department {
   // private id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -38,6 +38,13 @@ class AccountingDepartment extends Department {
     super(id, "Accounting");
   }
 
+  addEmployee(name: string): void {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -64,3 +71,6 @@ const accounting = new AccountingDepartment("d2", []);
 
 accounting.addReport("Something went wrong");
 accounting.printReports();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
