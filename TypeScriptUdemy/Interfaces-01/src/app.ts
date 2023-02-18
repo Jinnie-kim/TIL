4fac1df0997064638ced1400fd122d769f7d82b0
Jinnie <hjk7639@gmail.com>
2023-02-18 20:08:03 +0900

    TS private & public

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -2,6 +2,7 @@
 
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -10,12 +11,26 @@ class Department {
   describe(this: Department) {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "Sierra"; // 직접 추가
 console.log(accounting);
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "sierra", describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: "sierra", describe: accounting.describe };
+// accountingCopy.describe();
