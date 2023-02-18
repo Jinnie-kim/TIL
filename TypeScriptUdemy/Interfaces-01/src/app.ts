86b34c90d0096d60cb080e57879f7ec853627ae3
Jinnie <hjk7639@gmail.com>
2023-02-18 22:26:37 +0900

    TS abstract class

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -1,13 +1,13 @@
 // Code goes here!
 
 // 기본 클래스
-class Department {
+abstract class Department {
   static fiscalYear = 2020;
   // private id: string;
   // private name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
     // console.log(Department.fiscalYear); class안에서 사용하고 싶다면 클래스 이름을 써서 접근해야한다.
@@ -17,9 +17,11 @@ class Department {
     return { name: name };
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
+  abstract describe(this: Department): void;
+
+  // describe(this: Department) {
+  //   console.log(`Department (${this.id}): ${this.name}`);
+  // }
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -36,6 +38,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, "IT");
     this.admins = admins;
+  }
+
+  describe() {
+    console.log("IT Department - ID:", this.id);
   }
 }
 
@@ -59,6 +65,10 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
     this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log("Accounting Department  - ID:", this.id);
   }
 
   // you can override class's method 재정의할 수 있다.
@@ -100,7 +110,8 @@ const accounting = new AccountingDepartment("d2", []);
 accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong");
 console.log("lastReport", accounting.mostRecentReport);
-accounting.printReports();
+// accounting.printReports();
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
-accounting.printEmployeeInformation();
+// accounting.printEmployeeInformation();
+accounting.describe();
