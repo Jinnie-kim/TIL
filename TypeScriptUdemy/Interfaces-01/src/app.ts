0e8f00b9ad9f4cf78549bf8e45acb7a40901f6cb
Jinnie <hjk7639@gmail.com>
2023-02-18 21:35:25 +0900

    TS Getter & Setter

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -34,8 +34,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please  pass in a valid value");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   // you can override class's method 재정의할 수 있다.
@@ -48,6 +65,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -70,7 +88,9 @@ it.printEmployeeInformation();
 
 const accounting = new AccountingDepartment("d2", []);
 
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong");
+console.log("lastReport", accounting.mostRecentReport);
 accounting.printReports();
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
