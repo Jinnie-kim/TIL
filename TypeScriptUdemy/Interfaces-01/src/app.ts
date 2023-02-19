bc0a75d7b510b05332191a7b36ad3b564dc3643a
Jinnie <hjk7639@gmail.com>
2023-02-19 16:35:47 +0900

    TS optional parameters & properties

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -1,5 +1,17 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -7,15 +19,21 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable, Named {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(`${phrase} ${this.name}`);
+    if (this.name) {
+      console.log(`${phrase} ${this.name}`);
+    } else {
+      console.log("Hi");
+    }
   }
 }
 
@@ -28,7 +46,7 @@ class Person implements Greetable, Named {
 
 let user1: Greetable;
 
-user1 = new Person("Max");
+user1 = new Person();
 
 // user1.name = "hey";
 user1.greet("Hi there = I am");
