11a8ca3c949e02b3da4b9923a0e03be9d5af4211
Jinnie <hjk7639@gmail.com>
2023-02-26 20:51:28 +0900

    TS Type Guard

--- TypeScriptUdemy/Interfaces-01/src/app.ts
+++ TypeScriptUdemy/Interfaces-01/src/app.ts
@@ -1,6 +1,6 @@
 type Admin = {
   name: string;
-  privileges: string[];
+  previleges: string[];
 };
 
 type Employee = {
@@ -12,7 +12,7 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "Max",
-  privileges: ["create-server"],
+  previleges: ["create-server"],
   startDate: new Date(),
 };
 
@@ -20,3 +20,56 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add1(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    // type guard
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name :", emp.name);
+  if ("previleges" in emp) {
+    console.log("Privileges :", emp.previleges);
+  }
+  if ("startDate" in emp) {
+    console.log("start date :", emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
