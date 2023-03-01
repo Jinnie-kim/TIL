a616846c5c4fb192bc4d80e1f20761a9f60e6783
Jinnie <hjk7639@gmail.com>
2023-03-01 20:22:11 +0900

    TS Generic Functions

--- TypeScriptUdemy/Generics/src/app.ts
+++ TypeScriptUdemy/Generics/src/app.ts
@@ -1,11 +1,18 @@
-const names: Array<string> = []; // string[] 으로 지정하는 것과 100% 동일하다.
+// const names: Array<string> = []; // string[] 으로 지정하는 것과 100% 동일하다.
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T extends object, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj.name);
