7a0a055b2638d8914dec129cfb13815afbfcc9a2
Jinnie <hjk7639@gmail.com>
2023-03-02 10:45:20 +0900

    TS Accessor & Parameter Decorators

--- TypeScriptUdemy/Decorators/src/app.ts
+++ TypeScriptUdemy/Decorators/src/app.ts
@@ -38,11 +38,19 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
