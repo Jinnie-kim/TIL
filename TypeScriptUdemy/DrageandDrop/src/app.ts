9932d97fd02cee9d37445594558f6b8269fc8531
Jinnie <hjk7639@gmail.com>
2023-03-03 23:13:05 +0900

    Drag and Drop interface 생성 & ProjectItem에 draggble instance implements

--- TypeScriptUdemy/DrageandDrop/src/app.ts
+++ TypeScriptUdemy/DrageandDrop/src/app.ts
@@ -1,5 +1,17 @@
 // OOP TS code
 
+// Drag & Drop Interfaces
+interface Draggable {
+  drageStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 
 enum ProjectStatus {
@@ -159,7 +171,10 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
   private project: Project;
 
   get persons() {
@@ -178,7 +193,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent();
   }
 
-  configure() {}
+  @autobind
+  drageStartHandler(event: DragEvent): void {
+    console.log(event);
+  }
+
+  dragEndHandler(_: DragEvent): void {
+    console.log('DragEnd');
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.drageStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
 
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
