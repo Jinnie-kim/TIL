9fe59f3b9440a95c627ce4b8254834d6ba1d21ac
Jinnie <hjk7639@gmail.com>
2023-03-03 09:52:48 +0900

    autobind Decorator 추가

--- TypeScriptUdemy/DrageandDrop/src/app.ts
+++ TypeScriptUdemy/DrageandDrop/src/app.ts
@@ -1,4 +1,19 @@
 // OOP TS code
+
+// autobind decorator
+function autobind(_: any, _2: string, descriptior: PropertyDescriptor) {
+  const originalMethod = descriptior.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -35,13 +50,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
