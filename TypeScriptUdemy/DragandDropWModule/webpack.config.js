6547d0ce2052a6f5ff6a36e89dc7c72549f81ad0
Jinnie <hjk7639@gmail.com>
2023-03-04 23:50:30 +0900

    webpack config 설정 순서 수정

--- TypeScriptUdemy/DragandDropWModule/webpack.config.js
+++ TypeScriptUdemy/DragandDropWModule/webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -20,12 +27,5 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js'],
-  },
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
   },
 };
