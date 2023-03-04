2a3ca21bee95d6390de86d9e859817966a29982a
Jinnie <hjk7639@gmail.com>
2023-03-04 11:17:12 +0900

    프로젝트 state 로직 reference로 가져오기

--- TypeScriptUdemy/DrageandDropWNamespaces/src/app.ts
+++ TypeScriptUdemy/DrageandDropWNamespaces/src/app.ts
@@ -2,67 +2,10 @@
 
 /// <reference path='drag-drop-interfaces.ts' />
 /// <reference path='project-model.ts' />
+/// <reference path='project-state.ts' />
 
 namespace App {
   // Project Type
-
-  // Project State Management
-  // only one state management object for project with this singleton constructor now
-
-  type Listener<T> = (items: T[]) => void;
-
-  class State<T> {
-    protected listeners: Listener<T>[] = []; // list of function
-
-    addListener(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn);
-    }
-  }
-  class ProjectState extends State<Project> {
-    private projects: Project[] = []; // holds an array of projects
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    } // certain for singleton class
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      }
-      this.instance = new ProjectState();
-      return this.instance;
-    }
-
-    addProject(title: string, description: string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        ProjectStatus.Active
-      );
-      this.projects.push(newProject);
-      this.updateListeners();
-    }
-
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-      // basically switch the status of a project
-      const project = this.projects.find((prj) => prj.id === projectId);
-      if (project && project.status !== newStatus) {
-        project.status = newStatus;
-        this.updateListeners();
-      }
-    }
-
-    private updateListeners() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.projects.slice());
-      }
-    }
-  }
-
-  const projectState = ProjectState.getInstance();
 
   // Validation
   interface Validatable {
