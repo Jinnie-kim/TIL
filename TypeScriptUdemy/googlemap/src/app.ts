ebd09ef54b3f70bca7ad5090b6b620c9901d0a16
Jinnie <hjk7639@gmail.com>
2023-03-05 15:44:04 +0900

    Google api key 설정하기

--- TypeScriptUdemy/googlemap/src/app.ts
+++ TypeScriptUdemy/googlemap/src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyCbD5HDBB3Gy5u3qTrRFDaZCSQ-DGhg148';
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
