e869ab0be5b280a68c30a5e2f1fd414b5105da7d
Jinnie <hjk7639@gmail.com>
2023-03-07 21:32:28 +0900

    로그아웃 액션 추가 & authState에 따라 list 메뉴 on and off

--- ReactUdemy/react-redux/src/components/Header.js
+++ ReactUdemy/react-redux/src/components/Header.js
@@ -1,20 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const authState = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const userLogoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {authState && (
+            <>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+              <li>
+                <button type="button" onClick={userLogoutHandler}>
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
