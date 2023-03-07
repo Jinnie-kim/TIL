64e0bec38de1f10e1a7b4baab4d280c0bed6397d
Jinnie <hjk7639@gmail.com>
2023-03-07 21:30:32 +0900

    사용자 인증 여부에 따라 auth or UserProfile 보여지는 컴포넌트 분리

--- ReactUdemy/react-redux/src/App.js
+++ ReactUdemy/react-redux/src/App.js
@@ -1,12 +1,16 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const authState = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
+      {authState ? <UserProfile /> : <Auth />}
       <Counter />
     </>
   );
