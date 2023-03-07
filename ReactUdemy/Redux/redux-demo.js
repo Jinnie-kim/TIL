bf9c7a77116fec77ea958634837f64251c654600
Jinnie <hjk7639@gmail.com>
2023-03-07 14:22:02 +0900

    리덕스 구독 함수 만들고 등록하기

--- ReactUdemy/Redux/redux-demo.js
+++ ReactUdemy/Redux/redux-demo.js
@@ -1,9 +1,16 @@
 const redux = require('redux');
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   return {
     counter: state.counter + 1,
   };
 };
 
-const store = redux.createStore(countReducer);
+const store = redux.createStore(counterReducer); // initialize but that doesn't trigger subscribtion
+
+const counterSubscriber = () => {
+  const latestState = store.getState(); // getState 메소드는 state가 업데이트된 후 최신 상태 스냅샷을 제공한다.
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber); // redux가 실행할거임
