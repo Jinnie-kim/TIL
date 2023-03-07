81717ddafe7ba4e6eb1956687b7bd2bfbefe8e77
Jinnie <hjk7639@gmail.com>
2023-03-07 15:27:19 +0900

    리액트 컴포넌트에서 리덕스 store가 관리하는 데이터 사용하기 useSelector

--- ReactUdemy/react-redux/src/components/Counter.js
+++ ReactUdemy/react-redux/src/components/Counter.js
@@ -1,12 +1,14 @@
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter); // useSelector를 사용하면 자동으로 react-redux에 구독을 설정한다.
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
