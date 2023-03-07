cc4b9833578728b3a5ecd58bec7b96d4ed1e05ed
Jinnie <hjk7639@gmail.com>
2023-03-07 21:50:00 +0900

    Counter, Auth slice 파일 분리하기

--- ReactUdemy/react-redux/src/store/index.js
+++ ReactUdemy/react-redux/src/store/index.js
@@ -1,42 +1,6 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './coutner-slice';
+import authSlice from './auth-slice';
 
 const store = configureStore({
   reducer: {
@@ -45,8 +9,5 @@ const store = configureStore({
     auth: authSlice.reducer,
   },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
