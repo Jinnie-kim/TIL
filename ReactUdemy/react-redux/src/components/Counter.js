9e881d2fd7fa36e1bfad6fb6253fa596951fc977
Jinnie <hjk7639@gmail.com>
2023-03-07 16:15:16 +0900

    리덕스에서 복수의 state 다루기

--- ReactUdemy/react-redux/src/components/Counter.js
+++ ReactUdemy/react-redux/src/components/Counter.js
@@ -4,7 +4,10 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter); // useSelector를 사용하면 자동으로 react-redux에 구독을 설정한다.
-  const toggleCounterHandler = () => {};
+  const showCounter = useSelector((state) => state.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -21,7 +24,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
