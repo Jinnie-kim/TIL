b6919d15bd5b01d6efcfabcdc332af4cb928fcc7
Jinnie <hjk7639@gmail.com>
2023-03-08 19:34:04 +0900

    cartSlice, store reducer함수에 추가

--- ReactUdemy/react-redux-advanced/src/store/index.js
+++ ReactUdemy/react-redux-advanced/src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiSlice from './ui-slice';
+import cartSlice from './cart-slice';
 
 // const initialCartShowState = { showCart: false };
 
@@ -18,6 +19,7 @@ import uiSlice from './ui-slice';
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
