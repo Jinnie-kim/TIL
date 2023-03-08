f064115205d87dfefb0bfc4a55542d33a86585a1
Jinnie <hjk7639@gmail.com>
2023-03-08 15:47:30 +0900

    CarButton에 dispatch action 추가

--- ReactUdemy/react-redux-advanced/src/components/Cart/CartButton.js
+++ ReactUdemy/react-redux-advanced/src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { showCartActions } from '../../store';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const showCartHandler = () => {
+    dispatch(showCartActions.showCart());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={showCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
