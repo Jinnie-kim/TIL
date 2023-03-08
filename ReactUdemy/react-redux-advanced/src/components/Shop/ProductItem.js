828e16fd7ba635522c3160b668cf04870a402005
Jinnie <hjk7639@gmail.com>
2023-03-08 19:52:58 +0900

    addItem 액션을 전달하는 함수 Add to Cart 버튼 클릭 이벤트에 추가

--- ReactUdemy/react-redux-advanced/src/components/Shop/ProductItem.js
+++ ReactUdemy/react-redux-advanced/src/components/Shop/ProductItem.js
@@ -1,8 +1,21 @@
 import Card from '../UI/Card';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        price,
+        title,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +26,9 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button type="button" onClick={addItemHandler}>
+            Add to Cart
+          </button>
         </div>
       </Card>
     </li>
