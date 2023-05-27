ece5120d0a1996d4eeebcf548a223b8843032122
Jinnie <hjk7639@gmail.com>
2023-05-27 21:13:22 +0900

    타입스크립트 - 로딩 함수

--- typescript/2-types-projects-quiz/loading.ts
+++ typescript/2-types-projects-quiz/loading.ts
@@ -20,6 +20,12 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState) {
+    if (state.state === 'loading') return console.log(`👀 ${state.state}...`);
+    if (state.state === 'success') return console.log(`😃 ${state.response.body}`);
+    if (state.state === 'fail') return console.log(`😱 ${state.reason}`);
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
